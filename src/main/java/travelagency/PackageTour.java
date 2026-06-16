d26a8ea3c53e3dc9ed3ff2a76e5bbbcc685df863
Mario <codewithalbo@gmail.com>
2026-06-16 23:45:55 +0300

    Refactor PackageTour class and add getter method
    Removed unnecessary comments and added a getter for profit percentage.

--- src/main/java/travelagency/PackageTour.java
+++ src/main/java/travelagency/PackageTour.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package travelagency;
 
 import java.io.Serializable;
@@ -12,9 +8,9 @@ public class PackageTour implements Serializable {
     
     private int id;
     private String name;
-    private ArrayList<City> cities = new ArrayList<>(); // Sinthesh: lista me poleis
-    private ArrayList<Integer> nights = new ArrayList<>(); // Lista me dianukterefseis ana polh
-    private ArrayList<Travel> travels = new ArrayList<>(); // Polumorfikh sullogh metakinisewn
+    private ArrayList<City> cities = new ArrayList<>(); 
+    private ArrayList<Integer> nights = new ArrayList<>(); 
+    private ArrayList<Travel> travels = new ArrayList<>(); 
     private double profitPercentage;
 
     public PackageTour(int id, String name, double profitPercentage) {
@@ -25,6 +21,7 @@ public class PackageTour implements Serializable {
 
     public int getId() { return this.id; }
     public String getName() { return this.name; }
+    public double getProfitPercentage() { return this.profitPercentage; }
 
     public void addCity(City city, int numNights) {
         this.cities.add(city);
@@ -35,7 +32,6 @@ public class PackageTour implements Serializable {
         this.travels.add(travel);
     }
 
-    // Ypologismos kostous ( diamonh + metakinhseis )
     public double getCost() {
         double total = 0;
         for (int i = 0; i < cities.size(); i++) {
@@ -47,23 +43,21 @@ public class PackageTour implements Serializable {
         return total;
     }
 
-    // Kathorismos timhs me vash to pososto kerdous
     public double getPrice() {
         return this.getCost() * (1 + this.profitPercentage / 100);
     }
 
-    // Ektipwsh stoixeiwn paketou
     public void printPackage() {
         System.out.println("=== Touristiko paketo: " + name + " (ID: " + id + ") ===");
         System.out.println("Diadromh & Diamones:");
         for (int i = 0; i < cities.size(); i++) {
-            System.out.println(" • " + cities.get(i).getName() + " -> Dianukterefseis: " + nights.get(i));
+            System.out.println(" - " + cities.get(i).getName() + " -> Dianukterefseis: " + nights.get(i));
         }
         System.out.println("Sundeseis metakinhshs:");
         for (Travel t : travels) {
-            t.print(); // Polumorfikh klhsh
+            t.print(); 
         }
-        System.out.println("Kostos Paketou: " + getCost() + "€ | Timh pwlhshs: " + getPrice() + "€");
+        System.out.println("Kostos Paketou: " + getCost() + "$ | Timh pwlhshs: " + getPrice() + "$");
         System.out.println("----------------------------------------");
     }
 }
